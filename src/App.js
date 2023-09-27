85711686e5b028dcc9860f6a2ba40b4e0b38da68
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-09-27 17:37:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   }, []);
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/login' element={<Login />} />
@@ -44,7 +44,8 @@ function App() {
           <Route path='/contact' element={<Contact />} />
           <Route path='/faq' element={<FAQPage />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      Site is down for maintainance
     </>
   );
 }
